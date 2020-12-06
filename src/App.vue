7d5125a157e204f7efef609f2b4b661e2c1902f0
kpassosg <kpassosg@outlook.com>
2020-12-05 19:33:55 -0500

    Encabezado, Banner y seccion de servicios realizados en VUE

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
+   <encabezado-banner></encabezado-banner>
+   <seccion-servicios></seccion-servicios>
   </div>
 </template>
 
 <script>
+import EncabezadoBanner from './components/EncabezadoBanner.vue'
+import SeccionServicios from './components/SeccionServicios.vue'
 
 export default {
   name: 'App',
+  components: {
+    EncabezadoBanner,
+    SeccionServicios
+  }
 }
 </script>
 

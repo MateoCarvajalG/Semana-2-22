2d1ddb2a764939ec3d579857aecc2fbf696dc7ab
CristianCT <cridejecato@gmail.com>
2020-12-05 19:12:08 -0500

    Seccion de miembros con Vue CLI

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,90 @@
 <template>
   <div id="app">
+    <h2>{{ titulo }}</h2>
+    <div class="card-deck">
+      <div class="card"  v-for = "(item , index) of team" :key = "index">
+        <TeamCard v-bind:member = "item" /> 
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
-export default {
-  name: 'App',
-}
+  import TeamCard from './components/TeamCard.vue'
+
+  export default {
+    name: 'App',
+    components: {TeamCard},
+    
+    data () {
+
+      return{
+
+        titulo : 'EQUIPO',
+        team: [{
+          codigo: 1,
+          nombre: 'FRANCISCO ASTUDILLO',
+          descripcion: 'Francisco Astudillo, quien actualmente es residente de la ciudad de Cali. Graduado como Ingeniero Ambiental en la Universidad del Valle y por ultimo, pero no menos importante, su deporte favorito es BMX.',
+          rol: 'Desarrollador backend',
+          image: "./img/Francisco-Astudillo.jpg"
+        },
+        
+        {
+          codigo: 2,
+          nombre: 'JULIAN JIMENEZ ORTIZ',
+          descripcion: 'Julian Jimenez Ortiz, Ingeniero Geologo graduado en la Universidad Nacional de Colombia, actualmente se encuentra viviendo en la ciudad de Florencia, donde aprovecha el tiempo para practicar piano, manejar bicicleta y juagr volley.',
+          rol: 'Desarrollador backend',
+          image: './img/Julian-Jimenez-Ortiz.png'
+        },
+
+        {
+          codigo: 3,
+          nombre: 'KEWIN PASSOS G.',
+          descripcion: 'Kewin Passos G. graduado de la Universidad Nacional de Colombia como Ingeniero Forestal y que actualmente es residente de la ciudad de Medellin, hoy en dia dedica parte de su tiempo a desarrollar actividades para cumplir la mision MINTIC 2022.',
+          rol: 'Desarrollador backend',
+          image: './img/Kewin-Passos-G.jpg'
+        },
+
+        {
+          codigo: 4,
+          nombre: 'MATEO CARVAJAL',
+          descripcion: 'Mateo Carvajal, un amante de los motores y la tecnologia, logro graduarse como Ingeniero Mecatronico de la Universidad de Caldas, ademas de ser tecnologo en mecatronica automotriz con tan solo 25 años.',
+          rol: 'Desarrollador backend',
+          image: './img/Mateo-Carvajal.png'
+        },
+
+        {
+          codigo: 5,
+          nombre: 'CRISTIAN CABARCAS',
+          descripcion: 'Cristian Cabarcas, quien actualmente es residente de la ciudad de Cartagena, se encuentra siguiendo los pasos de sus compañeros para lograr una profesion al igual que ellos, dedicando su tiempo al estudio.',
+          rol: 'Desarrollador backend',
+          image: './img/Cristian-Cabarcas.jpg'
+        }]
+      }
+    }
+  }
+  
 </script>
 
+<style>
+  .card-deck{
+
+    margin-left: 0px;
+    margin-right: 0px;
+  }  
+
+  .card{
+
+    height: 100%;
+    border-radius: 10px;
+  }
+
+  h2{
+    margin-top: 5px;
+    text-align: center;
+    background: rgba(11, 51, 1, 0.507);
+    border-radius: 20px;
+  }
+
+</style>

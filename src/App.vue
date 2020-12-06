73d0497cc5c1a0ae30e2b4a20c4b79d1cbaea677
FcoKali <pachokali@hotmail.com>
2020-12-06 09:38:13 -0500

    creacion componente footer y llmado desde App vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <pagefooter></pagefooter>
   </div>
 </template>
 
 <script>
+import Pagefooter from './components/Pagefooter.vue'
 
 export default {
   name: 'App',
+  components:{
+    Pagefooter
+  }
 }
 </script>
 
---
+++ src/components/Pagefooter.vue
@@ -0,0 +1,61 @@
+<template>
+    <footer class="page-footer bg-secondary text-white pt-4">
+
+        <!-- Footer Text -->
+        <div class="container-fluid text-center text-md-left">
+
+            <!-- Grid row -->
+            <div class="text-center row">
+
+                <!-- Grid column -->
+                <div class="col-md-6 mt-md-0 mt-3">
+
+                    <!-- Content -->
+                    <h5 class="text-uppercase font-weight-bold">Información pagina web</h5>
+                    <p>Tienda deportiva especializada. Importadores directos de las mejores marcas deportivas </p>
+                    <a href="https://github.com/Grupo22-mintic/workshop1/settings/access?query=filter%3Acollaborators">  GitHub Grupo 22</a>
+
+                </div>
+                <!-- Grid column -->
+
+                <hr class="clearfix w-100 d-md-none pb-3">
+
+                <!-- Grid column -->
+                <div class="col-md-6 mb-md-0 mb-3">
+
+                    <!-- Content -->
+                    <h5 class="text-uppercase font-weight-bold">Información de contactos</h5>
+                    <p>Mateo Carvajal, seccion superior. GitHub: <a href="https://github.com/MateoCarvajalG">  MateoCarvajalG </a>, </p>
+                    <p>Julián Jiménez Ortiz, seccion de servicios. GitHub: <a href="https://github.com/behindafact"> behindafact </a> </p>
+                    <p>Kewin Passos G, seccion noticias. GitHub: <a href="https://github.com/kpassosg">  kpassosg </a> </p>
+                    <p>Cristian Cabarcas, seccion de equipo. GitHub: <a href="https://github.com/CristianCT">  CristianCT </a> </p>
+                    <p>Francisco Astudillo, seccion Footer. GitHub: <a href="https://github.com/Fcokali"> FcoKali </a> </p>
+
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Footer Text -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-1 bg-dark">© 2020 Copyright: Grupo 22
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Pagefooter',
+
+}
+
+</script>
+
+<style  scoped>
+
+</style>
